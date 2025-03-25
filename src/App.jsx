12b63226c7c8276f60f968318ac710b1d81f0f50
Gleb <mamaka.s.pistoletom@gmail.com>
2025-03-25 16:28:08 +0300

    initialization project

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
-const Card=()=>{
-    return(
-        <><h2>Movies</h2></>
-    )
-}
-const App=()=>{
-return(
-    <>
-        <h2>Functional Arrow Component</h2>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-    </>
-)
-}
+const App = () => {
+    return (
+        <main>
+            <div className={'pattern'}/>
+            <div className={'wrapper'}>
+                <header>
+                    <img src='../public/hero.png' alt='Hero Banner'></img>
+                    <h1>Find <span className={'text-gradient'}>Movies</span> You`ll Enjoy Without Hassle</h1>
+                </header>
+            </div>
+        </main>
+    );
+};
 
-export default App
+export default App;
